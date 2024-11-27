25ddefbb775e685e0d3114d72e6697d3116363b8
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-11-26 20:46:17 -0700

    checked

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div> Welcome to Book Review App </div>;
+  return <div> Welcome to Book Review App -- By Trupesh Mandani </div>;
 }
