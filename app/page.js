847bb87b963a75dd3ffd2f95493e270c71e15bd1
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-11-27 15:08:08 -0700

    Project pages and component setup and search bar created

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import SearchBar from "./Components/SearchBar";
 
 export default function Home() {
   return (
     <div className=" text-5xl flex h-screen items-center justify-center">
       {" "}
-      Welcome to Book Review App -- By Trupesh Mandani{" "}
+      Welcome to Book Review App -- By Trupesh Mandani <SearchBar />
     </div>
   );
 }
