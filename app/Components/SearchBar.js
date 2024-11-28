4247b5e9a38efc2e02b10c4fd1fc01bae97e40e5
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-11-27 17:39:30 -0700

    Final Alignment changes made. . . . Nav and seachbae done

--- app/Components/SearchBar.js
+++ app/Components/SearchBar.js
@@ -5,15 +5,16 @@ import { FaSearch } from "react-icons/fa";
 
 const SearchBar = () => {
   return (
-    <div className="brutalist-container">
+    <div className="relative w-[250px] font-mono">
       <input
         placeholder="SEARCH HERE"
-        className="brutalist-input smooth-type"
+        className="w-full p-4 text-xl font-bold text-black bg-white border-4 border-black outline-none transition-all duration-300 ease-in-out shadow-[5px_5px_0px_#000,10px_10px_0px_#4a90e2]"
         type="text"
       />
-      <FaSearch className="absolute right-7 top-1/2 transform size-4 -translate-y-1/2 text-black" />
+      <FaSearch className="absolute right-7 top-1/2 transform -translate-y-1/2 text-black text-2xl" />
     </div>
   );
 };
 
 export default SearchBar;
+// corect code
--- app/page.js
+++ app/page.js
@@ -5,10 +5,15 @@ import NavBar from "./Components/NavBar";
 export default function Home() {
   return (
     <div className="bg-black text-white min-h-screen">
-      <div className="flex justify-between items-center w-full p-4">
-        <NavBar />
-        <SearchBar />
+      <div className="flex justify-between items-start w-full p-4">
+        <div className=" w-full">
+          <NavBar />
+        </div>
+        <div className=" px-5 py-1">
+          <SearchBar />
+        </div>
       </div>
     </div>
   );
 }
+// correct code
